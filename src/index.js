fd6b16284c4b9d76cd1e40864e5c021356f67b58
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-08-15 13:13:35 +0530

    styling in progress

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import "./style.css";
-import forecast from "./weather";
+// import forecast from "./weather";
 
-let location='delhi';
-forecast(location);
+// let location='italy';
+// forecast(location);
 
-// try {
-//     const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/bengaluru/next7days?unitGroup=metric&key=D6FJHHE29KMGZ47KUBVCVUJ9J&contentType=json", { mode: 'cors' });
-//     const weatherData = await response.json();
-//     console.log(weatherData);
-// } catch (err) {
-//     console.error(err);
-// }
+
   
   
--- src/weather.js
+++ src/weather.js
@@ -3,11 +3,13 @@ async function forecast(location) {
     let response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next5days?unitGroup=metric&key=D6FJHHE29KMGZ47KUBVCVUJ9J&contentType=json`
     );
+    if(!response.ok){
+      throw new Error(`Place: ${location} not found`);      
+    }
     let weatherData = await response.json();
     console.log(weatherData);
   } catch (err) {
-    console.error('place not found',err);
-    // console.log('place not found');
+    console.error(err.message);
   }
 }
 
